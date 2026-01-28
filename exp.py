da5bb8b0895232ef287a5570f4dfbbb43065e159
REDsage8390 <bhambererushikesh4@gmail.com>
2026-01-28 13:59:49 +0530

    2nd

---
+++ exp.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Dummy credentials
+VALID_USER = "admin"
+VALID_PASS = "admin123"
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == VALID_USER and password == VALID_PASS:
+            return redirect(url_for("dashboard"))
+        else:
+            error = "Invalid username or password"
+
+    return render_template("login.html", error=error)
+
+
+@app.route("/dashboard")
+def dashboard():
+    return "<h1>Login Successful 🎉</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
